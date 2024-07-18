21d91dfc461b3de9d8abad7907fa1b59412b7776
Tusshar <tpaul.be21@gmail.com>
2024-07-18 23:33:46 +0530

    add global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,19 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import express from "express";
+import globalErrorHandler from "./middlewares/globalErrorHandler";
 
 const app = express();
 
 app.get("/", (req, res, next) => {
+    // throw new Error("This is an error");
+    // const error = createHttpError(400, "something went wrong");
+    // throw error;
     res.json({ message: "Hello World!" });
-    next();
+    
 })
+
+//global error handling
+
+app.use(globalErrorHandler);
 
 export default app;
